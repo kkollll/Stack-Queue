add4432922e7e49c6ad133bfbbfcf2bc532e1f59
Wr2020 <comin_008@163.com>
2020-05-20 00:00:08 +0800

    有效的括号

---
+++ src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (c == ')' || c == ']' || c == '}') {
+                    char topChar = stack.pop();
+                    if (c == ')' && topChar != '(') {
+                        return false;
+                    }
+                    if (c == ']' && topChar != '[') {
+                        return false;
+                    }
+                    if (c == '}' && topChar != '{') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isValid("{哈哈()[阿松大]}"));
+    }
+}
